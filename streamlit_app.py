503b4623406d34bf95f6bf602355e6f29f437c87
MellyMel Melnyk <106625634+MaruFernandez@users.noreply.github.com>
2022-06-16 16:34:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,2 @@
 import streamlit
 streamlit.title ('My Moms New healthy dinner')
-
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & blueberry oatmeal')
-streamlit.text('🥗 Kale, spinach & Rocket Smoothie')
-streamlit.text('🐔 Hared-Boiled Free-Range egg')
-streamlit.text('🥑🍞Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
